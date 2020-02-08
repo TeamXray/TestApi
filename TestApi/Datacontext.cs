3b0330a8d5019fa195c86d0f12788dd0d45a1fd0
mehri ali <alimehri1408@gmail.com>
2020-02-08 15:00:13 +0100

    test validation

---
+++ TestApi/Datacontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApi.Models;
+
+namespace TestApi
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<UsER>  Users {get; set;}
+    }
+}
---
+++ TestApi/Models/UsER.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi.Models
+{
+    public class UsER
+    { 
+
+        [Key]
+        public int id { get; set; }
+        public String name { get; set; }
+    }
+}
